5b299879984f29d28640ae29675c4a0446508787
unknown <shalahuddinalayubi24@gmail.com>
2022-01-22 09:36:37 +0800

    Template
    Create base template app.

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
     "./resources/**/*.blade.php",
     "./resources/**/*.js",
     "./resources/**/*.vue",
+    "./packages/ngeblog/framework/src/Ngeblog/**/*.blade.{html,php,js,vue}"
   ],
   theme: {
     extend: {},
