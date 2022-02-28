e71d8623202a1e06e44b90281405db8becec1146
unknown <shalahuddinalayubi24@gmail.com>
2022-02-28 14:54:51 +0800

    Rename folder
    Rename framework to app because of more sensible.
    Rename database folder to match with psr-4.

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
     "./resources/**/*.blade.php",
     "./resources/**/*.js",
     "./resources/**/*.vue",
-    "./packages/ngeblog/framework/src/Ngeblog/**/*.blade.{html,php,js,vue}",
+    "./packages/ngeblog/app/src/Ngeblog/**/*.blade.{html,php,js,vue}",
     "./packages/alayubi/**/resources/js/**/*.{vue,js,ts,jsx,tsx}"
   ],
   theme: {
