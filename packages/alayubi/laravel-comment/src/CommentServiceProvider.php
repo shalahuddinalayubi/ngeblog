00eb97702e521acf1307184c1e7d40f75127d2a9
unknown <shalahuddinalayubi24@gmail.com>
2022-04-06 15:18:58 +0800

    Migrations
    Move the load of migrations to \Ngeblog\Comment\CommentServiceProvider

--- packages/alayubi/laravel-comment/src/CommentServiceProvider.php
+++ packages/alayubi/laravel-comment/src/CommentServiceProvider.php
@@ -35,8 +35,6 @@ class CommentServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies();
-        
-        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
 
         $this->loadViewsFrom(__DIR__.'/../resources/views', 'comment');
 
